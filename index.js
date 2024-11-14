f4f84d7931485255b7e4d7be7715b046cbf4a712
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-14 12:28:16 +0530

    Array : push & pop

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-"use strict";
-//Array declaration using Square Bracket
-let numbers = [1, 2, 3, 4, 5, 5];
+var numbers = [1, 2, 3, 4, 5, 5];
 console.log(numbers);
-//Array declaration using Array Constructor
-let numbers2 = new Array(1, 2, 3, 4, 5, 6);
-console.log(numbers2);
-//Array declaration using Array.of method
-let names = Array.of("Rushi", "Rohan");
-console.log(names);
+//Add element to an array
+numbers.push(100);
+console.log(numbers);
+//Remove element to an array
+numbers.pop();
+console.log(numbers);
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-//Array declaration using Square Bracket
 let numbers : number[] = [1, 2, 3, 4, 5, 5];
 console.log(numbers);
 
+//Add element to an array
+numbers.push(100);
+console.log(numbers);
 
-//Array declaration using Array Constructor
-let numbers2 : number[] = new Array(1, 2, 3, 4, 5, 6);
-console.log(numbers2);
+//Remove element to an array
+numbers.pop();
+console.log(numbers);
 
 
-//Array declaration using Array.of method
-let names : string[] = Array.of("Rushi", "Rohan");
-console.log(names);
+
 
 
