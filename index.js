3669c74bdec7b71643a3ad85b9aecba2a90700c4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-14 12:06:53 +0530

    Default & Optional parameter in function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var myName = "Rushikesh";
-var myRoll = 44;
-var result = true;
+"use strict";
+//Optional parameter denoted by "?"
+let greet = (name, id) => {
+    if (id) {
+        return `My name is ${name} & id is ${id}`;
+    }
+    else {
+        return `My name is ${name}`;
+    }
+};
+console.log(greet("Rushikesh"));
+console.log(greet("Rushikesh", 44));
+//Default parameter denoted by "="
+let greet2 = (name, id = 10) => {
+    if (id) {
+        return `My name is ${name} & id is ${id}`;
+    }
+    else {
+        return `My name is ${name}`;
+    }
+};
+console.log(greet2("Rushikesh"));
+console.log(greet2("Rushikesh", 44));
--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-let myName = "Rushikesh";
-let myRoll = 44;
-let result = true;
+//Optional parameter denoted by "?"
+let greet = (name : string , id?:number)=>{
+    if(id){
+        return `My name is ${name} & id is ${id}`;
+    }
+    else{
+        return `My name is ${name}`;
+    }
+}
+
+console.log(greet("Rushikesh"));
+console.log(greet("Rushikesh", 44));
+
+//Default parameter denoted by "="
+let greet2 = (name : string , id:number = 10)=>{
+    if(id){
+        return `My name is ${name} & id is ${id}`;
+    }
+    else{
+        return `My name is ${name}`;
+    }
+}
+
+console.log(greet2("Rushikesh"));
+console.log(greet2("Rushikesh", 44));
+
+
