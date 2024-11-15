019beb820d1c49f9599f93dbc69e8862cde33af7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-15 10:52:47 +0530

    Object in TS

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 "use strict";
-let numbers = [1, 2, 3, 4, 5, 5];
-//Map method
-let doubleValue = numbers.map((value) => value * 2);
-console.log(doubleValue);
-//Filter method
-let evenNumber = numbers.filter((value) => value % 2 == 0);
-console.log(evenNumber);
-//Reduce method
-let sum = numbers.reduce((sum, value) => sum += value);
-console.log(sum);
+const person = {
+    name: "Rushikesh",
+    age: 23,
+    address: {
+        city: "Pune",
+        country: "India"
+    }
+};
+console.log(person);
--- index.ts
+++ index.ts
@@ -1,17 +1,11 @@
-let numbers: number[] = [1, 2, 3, 4, 5, 5];
+const person : {name : string, age:number, address : {city : string, country : string}} = {
+    name : "Rushikesh",
+    age : 23,
+    address : {
+        city : "Pune",
+        country : "India"
+    }
+}
 
-//Map method
-let doubleValue : number[] = numbers.map((value: number) => value*2)
-console.log(doubleValue);
-
-
-//Filter method
-let evenNumber: number[] = numbers.filter((value)=>value%2==0);
-console.log(evenNumber);
-
-//Reduce method
-let sum : number = numbers.reduce((sum, value)=>sum+=value);
-console.log(sum);
-
-
+console.log(person);
 
